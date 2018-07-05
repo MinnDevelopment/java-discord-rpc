93644616c1743b1cf48e706ed01484d469a54a9f
Florian Spieß <business@minnced.club>
2018-07-05 12:23:57 +0200

    Update copyright to 2018

--- src/main/java/club/minnced/discord/rpc/package-info.java
+++ src/main/java/club/minnced/discord/rpc/package-info.java
@@ -1,5 +1,5 @@
 /*
- *     Copyright 2016 - 2017 Florian Spieß
+ * Copyright 2016 - 2018 Florian Spieß
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
